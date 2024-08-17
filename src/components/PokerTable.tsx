3770d64adc9941a2b0e7b06840f622c68ca49099
Jakub Durkáč <xdurkac1@fi.muni.cz>
2024-08-17 15:08:22 +0200

    Add wrapper element to contain poker table. Adjust styles.

--- src/components/PokerTable.tsx
+++ src/components/PokerTable.tsx
@@ -1,5 +1,9 @@
 const PokerTable = () => {
-  return <div className="poker-table">PokerTable</div>;
+  return (
+    <div className="poker-table-container">
+      <div className="poker-table">PokerTable</div>
+    </div>
+  );
 };
 
 export default PokerTable;
