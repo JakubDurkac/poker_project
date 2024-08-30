797ed32ae6d2b52a4a54af53c334ea27192ddcb7
Jakub Durkáč <xdurkac1@fi.muni.cz>
2024-08-30 18:00:21 +0200

    Move disconnect icon to folder public.

--- src/components/PokerTable.tsx
+++ src/components/PokerTable.tsx
@@ -45,7 +45,7 @@ const PokerTable = ({
         {pokerPlayerComponents}
         {isConnected && (
           <button className="disconnect-button" onClick={handleDisconnect}>
-            <img className="icon" src="/src/assets/disconnect_icon.png" />
+            <img className="icon" src="/public/disconnect_icon.png" />
           </button>
         )}
       </div>
